df48943023f2ad30ea8e5e6d1885f3aa8dc8fb4e
Sreeshanth T <sreeshanth@reizendretail.in>
2023-04-11 22:14:44 +0530

    # app init

--- README.md
+++
@@ -1,2 +0,0 @@
-# dj_docs
-To create documentation for django projects with docs-string
--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='dj_docs',
-    version='0.1',
-    description='Document creation library for Django',
-    author='Sreeshanth Thekkedath',
-    author_email='your_email@example.com',
-    url='https://github.com/yourusername/mylibrary',
-    packages=find_packages(),
-    install_requires=[
-        'Django>=3.0',
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-)
+setup()
